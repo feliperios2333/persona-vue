cf97d9a508d840b7531a4dfced79a4a350e565ed
Luis Morales <feliperios2003brs@gmail.com>
2025-05-21 19:37:28 -0500

    Configuracion libreria sweetalert2

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {faBank, faBuilding, faPencil, faPhone, faPlus, faTag, faTrash, faUser} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+library.add(faPhone);
+library.add(faUser);
+library.add(faBuilding);
+library.add(faBank);
+library.add(faPencil);
+library.add(faPlus);
+library.add(faTag);
+library.add(faTrash);
+
+createApp(App)
+.use(router)
+.mount('#app')
